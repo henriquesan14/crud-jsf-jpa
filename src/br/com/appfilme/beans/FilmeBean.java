2b60c5ae0fe4e0b18831849800a5a1525f98bb88
Henrique <ricogx@hotmail.com>
2019-10-06 13:37:31 -0300

    crud pronto

--- src/br/com/appfilme/beans/FilmeBean.java
+++ src/br/com/appfilme/beans/FilmeBean.java
@@ -5,21 +5,22 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
-import br.com.appfilme.dao.FilmeDao;
+import br.com.appfilme.dao.FilmeDaoImpl;
 import br.com.appfilme.models.Filme;
 
 @ManagedBean
 public class FilmeBean {
 
-	private FilmeDao filmeDao = new FilmeDao();
+	private FilmeDaoImpl filmeDao = new FilmeDaoImpl();
 	private Filme filme;
 	private List<Filme> filmes;
 	
 	@PostConstruct
 	public void init() {
 		filme = new Filme();
-		this.filmes = filmeDao.getAll();
+		getAll();
 	}
 
 	public Filme getFilme() {
@@ -37,6 +38,10 @@ public class FilmeBean {
 	public void setFilmes(List<Filme> filmes) {
 		this.filmes = filmes;
 	}
+	
+	public void selecionaFilme(Filme filme) {
+		this.filme = filme;
+	}
 
 	public void getAll(){
 		this.filmes = filmeDao.getAll();
@@ -44,11 +49,18 @@ public class FilmeBean {
 	
 	public void salvar() {
 		filmeDao.save(this.filme);
+		filme = new Filme();
+		getAll();
 	}
-	
 	
 	public void excluir(Long id) {
 		filmeDao.remove(id);
+		getAll();
+	}
+	
+	public void reset() {
+		System.out.println("reset");
+		this.filme = new Filme();
 	}
 	
 }
--- src/br/com/appfilme/dao/FilmeDao.java
+++ src/br/com/appfilme/dao/FilmeDao.java
@@ -2,41 +2,11 @@ package br.com.appfilme.dao;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
 import br.com.appfilme.models.Filme;
-import br.com.appfilme.utils.JPAUtils;
 
-public class FilmeDao {
-	
-	public List<Filme> getAll(){
-		EntityManager em  = JPAUtils.createEntityManager();
-		List<Filme> result = em.createQuery("SELECT f FROM Filme f", Filme.class).getResultList();
-		return result;
-	}
-	
-	
-	public void save(Filme filme) {
-		EntityManager em  = JPAUtils.createEntityManager();
-		em.getTransaction().begin();
-		if(filme.getId() == null) {
-			em.persist(filme);
-		}else {
-			em.merge(filme);
-		}
-		em.getTransaction().commit();
-		em.close();
-	}
-	
-	public void remove(Long id) {
-		EntityManager em = JPAUtils.createEntityManager();
-		em.getTransaction().begin();
-		Filme filme = em.find(Filme.class, id);
-		em.remove(filme);
-		em.getTransaction().commit();
-		em.close();
-	}
-	
-	
+public interface FilmeDao {
+	List<Filme> getAll();
+	void save(Filme filme);
+	void remove(Long id);
 
 }
